3b5e600b53b9648eaeb4d5be79ddcd0ac1705fca
Eric LAGRANGE <eric.lagrange75@gmail.com>
2023-06-11 18:47:55 +0200

    V1 communication between two machine with signalR with the feature Clipboard

---
+++ Hubs/CopyDataHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Host.Hubs
+{
+    public class CopyDataHub : Hub
+    {
+        public async Task CopyData(string message)
+        {
+            await Clients.All.SendAsync("DataCopied", message);
+        }
+
+        public async Task PasteData(string message)
+        {
+            await Clients.All.SendAsync("Data Copied", message);
+        }
+    }
+}
